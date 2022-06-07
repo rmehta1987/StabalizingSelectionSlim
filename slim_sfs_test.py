36a8f40219b213bc0d74119f5170b61416b93ffe
Rahul Mehta <rmehta197@gmail.com>
2022-06-07 12:40:38 -0500

    changed to torch binomiaL

--- slim_sfs_test.py
+++ slim_sfs_test.py
@@ -110,8 +110,19 @@ class SetTransformer(nn.Module):
 '''
 
 class integralDecoder(nn.Module):
-    def __init__(self):
+    def __init__(self, cuda=True, N=500, u=1e-7):
         super(integralDecoder, self).__init__()
+        
+        self.scaled_pop_mut = 4*N*u
+        if cuda:
+            self.points = torch.linspace(0.001, 1-.001, int(2*N)).cuda()
+            self.probs = torch.zeros(int((2*N))).cuda()
+        else:
+            self.points = torch.linspace(0.001, 1-.001, int(2*N))
+            self.probs = torch.zeros(int((2*N)))
+        
+        self.binomial_coef = torch.distributions.binomial.Binomial(total_count=N, probs=self.points) 
+            
     def forward(self, input, N, u, binomial_coef):
         """ Forward module for neural network to integrate the selection coefficient inferred and re-generate the site-frequency spectrum
 
@@ -124,14 +135,9 @@ class integralDecoder(nn.Module):
             _type_: _description_
         """        
         s = input
-        if s.is_cuda:
-            points = torch.linspace(0.001, 1-.001, int(2*N)).cuda()
-            probs = torch.zeros(int((2*N))).cuda()
-        else:
-            points = torch.linspace(0.001, 1-.001, int(2*N))
-            probs = torch.zeros(int((2*N)))  
+         
         
-        scaled_pop_mut = 4*N*u
+        
         #f_i = lambda i: binomial_coef[i] * ((1 - torch.exp(-2*N*s*(1-points))) * (torch.pow(points,i-1))*torch.pow((1-points),(N-i-1)) * torch.pow((1 - torch.exp(-2*N*s)),-1))
         #f_i = lambda i: (1 - torch.exp(-2*N*s*(1-points))) * (torch.pow(points,i-1))*torch.pow((1-points),(N-i-1)) * torch.pow((1 - torch.exp(-2*N*s)),-1)
         for i in range(1,int(2*N)-1):
@@ -190,9 +196,9 @@ class integralDecoder(nn.Module):
         
         return result_3                                                       
 
-class gwasVAE(nn.Module):
+class sfsVAE(nn.Module):
     """
-    The base VAE class containing linear encoder and decoder architecture.
+    The base VAE class containing linear encoder and decoder architecture for gwas Site Frequency Spectrums.
     Can be used as a base class for VAE's with normalizing flows.
     """
 
@@ -204,7 +210,7 @@ class gwasVAE(nn.Module):
             num_heads: an integer, number of heads.
             ln: boolean to normalize layers
         """
-        super(gwasVAE, self).__init__()
+        super(sfsVAE, self).__init__()
         self.z_size = latent_dims # Latent dimensions
         self.input_size = int(pop_size*2) # Population size
         #self.num_heads = args.num_heads # Multi-attention head
@@ -315,7 +321,7 @@ class gwasVAE(nn.Module):
         z_mu, z_var = self.encode(x)
         # sample z
         z = self.reparameterize(z_mu, z_var)
-        x_mean = torch.clamp(self.decode(z), max=5)
+        x_mean = torch.clamp(self.decode(z), max=30)
         if x_mean.sum().isinf():
             print("x_mean is inf")
         if x_mean.sum().isnan():
@@ -388,21 +394,241 @@ class gwasVAE(nn.Module):
         else:
             return log_norm
         
-'''
-class slimExperiment(pl.LightningModule):
-    
-    def __init__(self,
-                 the_model: VAE,
-                 params: dict, dataset: LightningDataModule) -> None:
-        super(slimExperiment, self).__init__()
+   
+class effectSizeVAE(nn.Module):
+    """
+    The base VAE class containing linear encoder and decoder architecture for gwas Effect Sizes.
+    Can be used as a base class for VAE's with normalizing flows.
+    """
 
-        self.model = the_model
-        self.params = params
-        self.curr_device = None
-        self.hold_graph = False
-        self.dataset = dataset
-'''       
+    def __init__(self, latent_dims, pop_size, mut_rate, dim_hidden, use_set, use_cuda, coef):
+        """_summary_
+
+        Arguments:
+            dim_in: an integer, input dimension.
+            num_inds: an integer, number of inducing points.
+            num_heads: an integer, number of heads.
+            ln: boolean to normalize layers
+            latent_dims (_type_): _description_
+            pop_size (_type_): _description_
+            mut_rate (_type_): _description_
+            dim_hidden (_type_): _description_
+            use_set (_type_): _description_
+            use_cuda (_type_): _description_
+            coef (_type_): _description_
+            
+        """
+        super(effectSizeVAE, self).__init__()
+        self.z_size = latent_dims # Latent dimensions
+        self.input_size = int(pop_size*2) # Population size
+        #self.num_heads = args.num_heads # Multi-attention head
+        #self.num_inds = args.num_inds
+        self.dim_hidden = dim_hidden
+        self.set = use_set # If using permutation invariant layers (SET) 
+        #self.q_z_nn_output_dim = 256
+        
+        self.pop_size = pop_size
+        self.mut_rate = mut_rate
+        self.q_z_nn, self.q_z_mean, self.q_z_var, self.q_s_nn, self.q_s_mean, self.q_s_var, self.q_omega_nn, q_omega_mean, self.q_omega_var = self.create_encoder()
+        self.p_x_nn = self.create_decoder()
+        
+        #self.ln = args.ln
+        #self.num_outputs = args.num_outputs # number of samples
+        
+
+        # auxiliary
+        if use_cuda:
+            self.FloatTensor = torch.cuda.FloatTensor
+            self.binomial_coef = torch.from_numpy(coef).cuda()
+        else:
+            self.FloatTensor = torch.FloatTensor
+            self.binomial_coef = torch.from_numpy(coef)
+
+        # log-det-jacobian = 0 without flows
+        self.log_det_j = self.FloatTensor(1).zero_()
+
+    def create_encoder(self):
+        """
+        Helper function to create the elemental blocks for the encoder. Creates a set based encoder
+        as the counts of sites and independant with each other.  
+        """
+        if self.set:
+            q_z_nn = nn.Sequential(ISAB(self.input_size, self.dim_hidden, self.num_heads, self.num_inds, self.ln), 
+                                            ISAB(self.dim_hidden, self.dim_hidden, self.num_heads, self.num_inds, self.ln))
+            q_z_mean = nn.Linear(256, self.z_size)
+            q_z_var = nn.Sequential(
+                nn.Linear(256, self.z_size),
+                nn.Softplus(),
+                nn.Hardtanh(min_val=0.01, max_val=7.))
+        else:
+            q_z_nn = nn.Sequential(nn.Linear(self.input_size, self.dim_hidden),nn.Linear(self.dim_hidden, self.dim_hidden),
+                                   nn.Linear(self.dim_hidden, self.dim_hidden))  # 3 Layers 
+            
+            q_z_mean = nn.Linear(self.dim_hidden, self.z_size)
+                
+            q_z_var = nn.Sequential(
+                nn.Linear(self.dim_hidden, self.z_size),
+                nn.Softplus(),
+                nn.Hardtanh(min_val=0.01, max_val=7.))
+            
+            q_s_nn = nn.Sequential(nn.Linear(self.z_size, self.s_dim_hidden),nn.Linear(self.s_dim_hidden, self.s_dim_hidden),
+                                   nn.Linear(self.s_dim_hidden, self.s_dim_hidden))
+            q_s_mean = nn.Linear(self.s_dim_hidden, self.s_dim_hidden)
+            q_s_var = nn.Sequential(
+                nn.Linear(self.s_dim_hidden, self.s_dim_hidden),
+                nn.Softplus())
+            
+            q_omega_nn = nn.Sequential(nn.Linear(self.z_size, self.s_dim_hidden),nn.Linear(self.s_dim_hidden, self.s_dim_hidden),
+                                   nn.Linear(self.s_dim_hidden, self.s_dim_hidden))
+            q_omega_mean = nn.Linear(self.s_dim_hidden, 1)
+            q_omega_var = nn.Sequential(
+                nn.Linear(self.s_dim_hidden, 1),
+                nn.Softplus())
+             
+        return q_z_nn, q_z_mean, q_z_var, q_s_nn, q_s_mean, q_s_var, q_omega_nn, q_omega_mean, q_omega_var
+
+    def create_decoder(self):
+        """
+        Helper function to create the elemental blocks for the decoder. Creates a gated convnet decoder.
+        """
+        if self.set:
+            p_x_nn= nn.Sequential(
+                    PMA(self.dim_hidden, self.num_heads, self.num_outputs, ln=self.ln),
+                    SAB(self.dim_hidden, self.dim_hidden, self.num_heads, ln=self.ln),
+                    SAB(self.dim_hidden, self.dim_hidden, self.num_heads, ln=self.ln),
+                    nn.Linear(self.dim_hidden, self.input_size))
+            return p_x_nn
+        else:
+            p_beta = nn.Sequential(nn.Linear(self.s_dim_hidden+1, self.z_size),nn.Linear(self.z_size, self.dim_hidden),
+                                   nn.Linear(self.s_dim_hidden, self.s_dim_hidden), nn.Linear(self.dim_hidden, self.input_size))
+            
+            return p_beta
+
+   
+    def reparameterize(self, mu, var):
+        """
+        Samples z from a multivariate Gaussian with diagonal covariance matrix using the
+            reparameterization trick.
+        """
+
+        std = var.sqrt()
+        eps = self.FloatTensor(std.size()).normal_()
+        z = eps.mul(std).add_(mu)
+
+        return z
+
+    def encode(self, x):
+        """
+        Encoder expects following data shapes as input: shape = (batch_size, num_channels, width, height)
+        """
+
+        h = self.q_z_nn(x)
+        mean = self.q_z_mean(h)
+        var = self.q_z_var(h)
+        
+        return mean, var
+
+    def decode(self, s, omega):
+        """
+
+        """
+        x_mean = self.p_x_nn(torch.cat(s, omega))
+        
+        return x_mean
+
+    def forward(self, x):
+        """
+        Evaluates the model as a whole, encodes and decodes. Note that the log det jacobian is zero
+         for a plain VAE (without flows), and z_0 = z_k.
+        """
+
+        # mean and variance of z
+        z_mu, z_var = self.encode(x)
+        # sample z
+        z = self.reparameterize(z_mu, z_var)
+        
+        s_mu = self.q_s_mean(self.q_s_nn(z))
+        s_var = self.q_s_var(self.q_s_nn(z))
+        s_repram = self.reparameterize(s_mu, s_var)
+        
+        omega_mu = self.q_s_mean(self.q_s_nn(z))
+        omega_var = self.q_s_var(self.q_s_nn(z))
+        omega_repram = self.reparameterize(omega_mu, omega_var)
+        
+        x_mean = self.decode(s_repram, omega_repram)
+        if x_mean.sum().isinf():
+            print("x_mean is inf")
+        if x_mean.sum().isnan():
+            print("x_mean is inf")
+        
+        return x_mean, z_mu, z_var, self.log_det_j, z, z
     
+    def calculate_loss(self, recon_x, x, z_mu, z_var, z_0, z_k, ldj):
+        """
+        Computes the binary loss function while summing over batch dimension, not averaged!
+        :param recon_x: shape: (batch_size, num_channels, pixel_width, pixel_height), bernoulli parameters p(x=1)
+        :param x: shape (batchsize, num_channels, pixel_width, pixel_height), pixel values rescaled between [0, 1].
+        :param z_mu: mean of z_0
+        :param z_var: variance of z_0
+        :param z_0: first stochastic latent variable
+        :param z_k: last stochastic latent variable
+        :param ldj: log det jacobian
+        :param beta: beta for kl loss
+        :return: loss, ce, kl
+        """
+        
+        beta=1.
+
+        #reconstruction_function = nn.BCELoss(reduction='sum')
+        reconstruction_function = nn.PoissonNLLLoss(full=True, reduction='mean')
+
+        batch_size = x.size(0)
+
+        # - N E_q0 [ ln p(x|z_k) ]
+        bce = reconstruction_function(recon_x, x)
+
+        # ln p(z_k)  (not averaged)
+        log_p_zk = self.log_normal_standard(z_k, dim=1)
+        # ln q(z_0)  (not averaged)
+        log_q_z0 = self.log_normal_diag(z_0, mean=z_mu, log_var=z_var.log(), dim=1)
+        # N E_q0[ ln q(z_0) - ln p(z_k) ]
+        summed_logs = torch.sum(log_q_z0 - log_p_zk)
+
+        # sum over batches
+        summed_ldj = torch.sum(ldj)
+
+        # ldj = N E_q_z0[\sum_k log |det dz_k/dz_k-1| ]
+        kl = (summed_logs - summed_ldj)
+        loss = bce + beta * kl
+
+        loss = loss / float(batch_size)
+        bce = bce / float(batch_size)
+        kl = kl / float(batch_size)
+
+        return loss, bce, kl
+    
+    def log_normal_standard(self, x, average=False, reduce=True, dim=None):
+        log_norm = -0.5 * x * x
+
+        if reduce:
+            if average:
+                return torch.mean(log_norm, dim)
+            else:
+                return torch.sum(log_norm, dim)
+        else:
+            return log_norm
+    
+    def log_normal_diag(self, x, mean, log_var, average=False, reduce=True, dim=None):
+        log_norm = -0.5 * (log_var + (x - mean) * (x - mean) * log_var.exp().reciprocal())
+        if reduce:
+            if average:
+                return torch.mean(log_norm, dim)
+            else:
+                return torch.sum(log_norm, dim)
+        else:
+            return log_norm
+
+
 
 def main(argv):
 
@@ -421,6 +647,7 @@ def main(argv):
         device = torch.device("cuda" if FLAGS.cuda else "cpu")
         train_loss = np.zeros(len(data_loader))
         train_z= np.zeros(len(data_loader))
+        train_z_mu =  np.zeros(len(data_loader))
         x_clamp=10
         print("Start Training")
         for i in range(FLAGS.Epochs):
@@ -430,16 +657,18 @@ def main(argv):
                     #create a predefined binomial sample
                     # initalize model
                     #(self, latent_dims, pop_size, mut_rate, dim_hidden, use_set, use_cuda):
-                    model = gwasVAE(FLAGS.Latent_Dim, pop_size=the_data['Labels']['pop_size'][0].item(), 
+                    model = sfsVAE(FLAGS.Latent_Dim, pop_size=the_data['Labels']['pop_size'][0].item(), 
                                     mut_rate=the_data['Labels']['mut_rate'][0], dim_hidden=FLAGS.Hidden_Dim, use_set=False, use_cuda=FLAGS.cuda, coef = binom_coef).to(device)
                     true_sel_coef = the_data['Labels']['sel_coef'][0]
                     optimizer = optim.Adamax(model.parameters(), lr=0.0005,  eps=1.e-7)
                     if FLAGS.cuda:
                         #data = torch.mul(the_data['Freq'][:,:,1].cuda(), (1/2*the_data['Labels']['pop_size'][0]))
                         data = the_data['Freq'][:,:,1].cuda()
+                        beta_data = the_data['Effect'][:].cuda()
                     else:
                         #data = torch.mul(the_data['Freq'][:,:,1], (1/2*the_data['Labels']['pop_size'][0]))
                         data = the_data['Freq'][:,:,1]
+                        beta_data = the_data['Effect'][:].cuda()
                     
                     x_mean, z_mu, z_var, ldj, z0, zk = model(data)
 
@@ -448,6 +677,7 @@ def main(argv):
                     loss.backward()
                     train_loss[step] = loss.item()
                     train_z[step] = torch.mean(zk).detach().cpu().numpy()
+                    train_z_mu[step] = torch.mean(z_mu).detach().cpu().numpy()
                     optimizer.step()
                     num_data += 2 # hard-coded temporarily
                 else:
@@ -455,9 +685,11 @@ def main(argv):
                     if FLAGS.cuda:
                         #data = torch.mul(the_data['Freq'][:,:,1].cuda(), (1/2*the_data['Labels']['pop_size'][0]))
                         data = the_data['Freq'][:,:,1].cuda()
+                        beta_data = the_data['Effect'][:].cuda()
                     else:
                         #data = torch.mul(the_data['Freq'][:,:,1], (1/2*the_data['Labels']['pop_size'][0]))
                         data = the_data['Freq'][:,:,1]
+                        beta_data = the_data['Effect'][:].cuda()
                     x_mean, z_mu, z_var, ldj, z0, zk = model(data)
                     #loss, rec, kl, bpd = model.calculate_loss(x_mean, data, z_mu, z_var, z0, zk, ldj)
                     loss, rec, kl = model.calculate_loss(x_mean, data, z_mu, z_var, z0, zk, ldj)
@@ -465,6 +697,7 @@ def main(argv):
                     optimizer.step()
                     train_loss[step] = loss.item()
                     train_z[step] = torch.mean(zk).detach().cpu().numpy()
+                    train_z_mu[step] = torch.mean(z_mu).detach().cpu().numpy()
                     num_data += 2 # hard-coded temporarily
                 
                 if step % 10 == 0:
@@ -473,23 +706,38 @@ def main(argv):
                         i, num_data, len(data_loader.sampler), 100. * step / len(data_loader),
                         loss.item(), rec, kl))
                     #break
-            plt.axhline(y=np.log(np.abs(true_sel_coef.numpy())), color='r', linestyle='-')
-            plt.plot(np.log(np.abs(train_z)))
-            plt.ylabel('Selection Coefficient')
+            plt.axhline(y=np.log(np.abs(true_sel_coef.numpy()*the_data['Labels']['pop_size'][0].numpy()*2)), color='r', linestyle='-')
+            plt.plot(np.log(np.abs(train_z_mu)), label='Training Epoch {}'.format(i))
+            plt.ylabel('Log Selection Coefficient')
             plt.xlabel('Training Step')
-            plt.title('Inferred Selection from Simulated GWAS with x clamped at {}'.format(x_clamp))
-            plt.savefig('Slim_experiment_2_1_75_epoch_{}'.format(i))
+            plt.title('Inferred Selection from s_mu Simulated GWAS with x clamped at {}'.format(x_clamp))
+            plt.legend(loc='upper right')
+            plt.savefig('Slim_experiment_6_1_75_epoch_{}'.format(i))
 
                     
     else:
         print("Need a path to a dataset or to create one.")
     
-    plt.axhline(y=np.log(np.abs(true_sel_coef.numpy())), color='r', linestyle='-')
+    plt.axhline(y=np.log(np.abs(true_sel_coef.numpy()*the_data['Labels']['pop_size'][0].numpy()*2)), color='r', linestyle='-')
     plt.ylabel('Selection Coefficient')
     plt.xlabel('Training Step')
-    plt.title('Inferred Selection from Simulated GWAS with x clamped at {}'.format(x_clamp))
+    plt.title('Inferred Selection from s_k Simulated GWAS with x clamped at {}'.format(x_clamp))
+    legend_labels = ['Epoch {}'.format(j) for j in range(1,11)]
+    plt.legend(legend_labels, loc='upper right')
     plt.plot(np.log(np.abs(train_z)))
-    plt.savefig('Slim_2_all_experiment_2_1_75_epoch'.format(i))
+    plt.savefig('Slim_4_all_experiment_6_1_75_epoch'.format(i))
+    plt.close() 
+    
+    plt.axhline(y=np.log(np.abs(true_sel_coef.numpy()*the_data['Labels']['pop_size'][0].numpy()*2)), color='r', linestyle='-')
+    plt.ylabel('Selection Coefficient')
+    plt.xlabel('Training Step')
+    plt.title('Inferred Selection from s_mu Simulated GWAS with x clamped at {}'.format(x_clamp))
+    legend_labels = ['Epoch {}'.format(j) for j in range(1,11)]
+    plt.legend(legend_labels, loc='upper right')
+    plt.plot(np.log(np.abs(train_z_mu)))
+    plt.savefig('Slim_4_all_experiment_6_1_75_epoch'.format(i))
+    
+    
 
     
                 
